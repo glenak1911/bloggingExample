135fb7c068dcdcf72a7d1513238d62b6d10e9f07
Glen Knight <glenak1911@gmail.com>
2017-01-22 18:08:33 -0500

    Added MySQL Support

--- site/Models/Articles.js
+++ site/Models/Articles.js
@@ -24,6 +24,25 @@ client.connect(connection, function(err, database) {
     }
 }); 
 
+connection = mysql.createConnection({
+    host: mysql_host,
+    user: mysql_user,
+    password: mysql_password
+});
+connection.connect(function(err) {
+    if(err) {
+        throw new Error("Can't connect to MySQL.");
+    } else {
+        connection.query("USE " + mysql_database, function(err, rows, fields) {
+            if(err) {
+                throw new Error("Missing database.");
+            } else {
+                console.log("Successfully selected database.");
+            }
+        })
+    }
+});
+
 module.exports = function() {
     if(type == "mongodb") {
 	return {
@@ -55,11 +74,36 @@ module.exports = function() {
     		}
 	}
     } else {
-        return {
-            add: function(data, callback) { ... },
-            update: function(data, callback) { ... },
-            get: function(callback) { ... },
-            remove: function(id, callback) { ... }
-        }
+return {
+    add: function(data, callback) {
+        var date = new Date();
+        var query = "";
+        query += "INSERT INTO articles (title, text, date) VALUES (";
+        query += connection.escape(data.title) + ", ";
+        query += connection.escape(data.text) + ", ";
+        query += "'" + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "'";
+        query += ")";
+        connection.query(query, callback);
+    },
+    update: function(data, callback) {
+        var query = "UPDATE articles SET ";
+        query += "title=" + connection.escape(data.title) + ", ";
+        query += "text=" + connection.escape(data.text) + " ";
+        query += "WHERE id='" + data.id + "'";
+        connection.query(query, callback);
+    },
+    get: function(callback) {
+        var query = "SELECT * FROM articles ORDER BY id DESC";
+        connection.query(query, function(err, rows, fields) {
+            if(err) {
+                throw new Error("Error getting.");
+            } else {
+                callback(rows);
+            }
+        });
+    },
+    remove: function(id, callback) {
+        var query = "DELETE FROM articles WHERE id='" + id + "'";
+        connection.query(query, callback);
     }
 }
