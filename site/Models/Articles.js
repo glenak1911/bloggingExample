60f215276e6c0b0bae1e1312e21cf0b5c8f98f05
Glen Knight <glenak1911@gmail.com>
2017-01-22 17:45:50 -0500

    Added mongoDB functionality

--- site/Models/Articles.js
+++ site/Models/Articles.js
@@ -4,15 +4,56 @@ var crypto = require("crypto"),
     mongodb_host = "127.0.0.1",
     mongodb_port = "27017",
     collection;
-    
+
+var mysql = require('mysql'),
+    mysql_host = "127.0.0.1",
+    mysql_user = "root",
+    mysql_password = "",
+    mysql_database = "blog_application",
+    connection;
+
+connection = 'mongodb://';
+connection += mongodb_host + ':' + mongodb_port;
+connection += '/blog-application';
+client.connect(connection, function(err, database) {
+  if(err) {
+    throw new Error("Can't connect");
+  } else {
+    console.log("Connection to MongoDB server successful.");
+      collection = database.collection('articles');
+    }
+}); 
+
 module.exports = function() {
     if(type == "mongodb") {
-        return {
-            add: function(data, callback) { ... },
-            update: function(data, callback) { ... },
-            get: function(callback) { ... },
-            remove: function(id, callback) { ... }
-        }
+	return {
+  		add: function(data, callback) {
+    			var date = new Date();
+    			data.id = crypto.randomBytes(20).toString('hex');
+    			data.date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+    			collection.insert(data, {}, callback || function() {});
+  		},
+  		update: function(data, callback) {
+    			collection.update(
+            		{ID: data.id}, 
+            		data, 
+            		{}, 
+            		callback || function(){ }
+        		); 
+    		},
+    		get: function(callback) {
+        		collection.find({}).toArray(callback);
+    		},
+    		remove: function(id, callback) {
+        		collection.findAndModify(
+            		{ID: id}, 
+            		[], 
+            		{}, 
+            		{remove: true}, 
+            		callback
+        		);
+    		}
+	}
     } else {
         return {
             add: function(data, callback) { ... },
