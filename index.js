414610f0f158fd87437b19c123c6eb46b3a8acbf
Glen Knight <glenak1911@gmail.com>
2017-01-30 22:01:23 -0500

    updated routing and views

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var articles = require("./models/Articles")();
 
+
+app.set("username", "admin");
+app.set("password", "pass");
+app.use(express.cookieParser('blog-application'));
+app.use(express.session());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
@@ -11,8 +16,35 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(function(req, res, next) {
+    if((
+        req.session && 
+        req.session.admin === true
+    ) || (
+        req.body &&
+        req.body.username === app.get("username") &&
+        req.body.password === app.get("password")
+    )) {
+        req.logged = true;
+        req.session.admin = true;
+    };
+    next();
+});
+
+var protect = function(req, res, next) {
+    if(req.logged) {
+        next();
+    } else {
+        res.send(401, 'No Access.');
+    }
+}
+
 app.get('/api/get', require("./controllers/api/get"));
 app.get('/', require("./controllers/index"));
+app.post('/api/add', protect, require("./controllers/api/add"));
+app.post('/api/edit', protect, require("./controllers/api/edit"));
+app.post('/api/delete', protect , require("./controllers/api/delete"));
+app.all('/admin', require("./controllers/admin"));
 
 app.listen(3000);
 console.log('Listening on port 3000');
--- models/Articles.js
+++ models/Articles.js
@@ -1,109 +1,112 @@
 var crypto = require("crypto"),
     type = "mongodb",
+    // mongodb
     client = require('mongodb').MongoClient,
-    mongodb_host = "127.0.0.1",
-    mongodb_port = "27017",
-    collection;
-
-var mysql = require('mysql'),
+	mongodb_host = "MEANDev",
+	mongodb_port = "27017",
+	collection,
+    // mysql
+    mysql = require('mysql'),
     mysql_host = "127.0.0.1",
     mysql_user = "root",
     mysql_password = "",
-    mysql_database = "blog_application",
+    mysql_database = "nodejsblueprints",
     connection;
-
-connection = 'mongodb://';
-connection += mongodb_host + ':' + mongodb_port;
-connection += '/blog-application';
-client.connect(connection, function(err, database) {
-  if(err) {
-    throw new Error("Can't connect");
-  } else {
-    console.log("Connection to MongoDB server successful.");
-      collection = database.collection('articles');
-    }
-}); 
-
-connection = mysql.createConnection({
-    host: mysql_host,
-    user: mysql_user,
-    password: mysql_password
-});
-connection.connect(function(err) {
-    if(err) {
-        throw new Error("Can't connect to MySQL.");
-    } else {
-        connection.query("USE " + mysql_database, function(err, rows, fields) {
-            if(err) {
-                throw new Error("Missing database.");
-            } else {
-                console.log("Successfully selected database.");
-            }
-        })
-    }
-});
-
+    
 module.exports = function() {
     if(type == "mongodb") {
-	return {
-  		add: function(data, callback) {
-    			var date = new Date();
-    			data.id = crypto.randomBytes(20).toString('hex');
-    			data.date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-    			collection.insert(data, {}, callback || function() {});
-  		},
-  		update: function(data, callback) {
+        connection = 'mongodb://';
+        connection += mongodb_host + ':' + mongodb_port;
+        connection += '/blog-application';
+        client.connect(connection, function(err, database) {
+        	if(err) {
+        		throw new Error("Can't connect.");
+        	} else {
+        		console.log("Connection to MongoDB server successful.");
+          		collection = database.collection('articles');
+          	}
+        });
+        return {
+        	add: function(data, callback) {
+                var date = new Date();
+                data.id = crypto.randomBytes(20).toString('hex');
+        		data.date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+        		collection.insert(data, {}, callback || function() {});
+        	},
+        	update: function(data, callback) {
     			collection.update(
-            		{ID: data.id}, 
-            		data, 
-            		{}, 
-            		callback || function(){ }
-        		); 
-    		},
-    		get: function(callback) {
-        		collection.find({}).toArray(callback);
-    		},
-    		remove: function(id, callback) {
-        		collection.findAndModify(
-            		{ID: id}, 
-            		[], 
-            		{}, 
-            		{remove: true}, 
-            		callback
-        		);
-    		}
-	}
+                    {"id": data.id}, 
+                    {text: data.text, title: data.title, id: data.id},
+                    {}, 
+                    callback || function(){ }
+                );        
+            },
+            get: function(callback) {
+                collection.find({}).toArray(function(err, rows) {
+                    callback(rows)
+                });
+            },
+            remove: function(id, callback) {
+                collection.findAndModify(
+                    {id: id}, 
+                    [], 
+                    {}, 
+                    {remove: true}, 
+                    callback
+                );
+            }
+        }
     } else {
-return {
-    add: function(data, callback) {
-        var date = new Date();
-        var query = "";
-        query += "INSERT INTO articles (title, text, date) VALUES (";
-        query += connection.escape(data.title) + ", ";
-        query += connection.escape(data.text) + ", ";
-        query += "'" + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "'";
-        query += ")";
-        connection.query(query, callback);
-    },
-    update: function(data, callback) {
-        var query = "UPDATE articles SET ";
-        query += "title=" + connection.escape(data.title) + ", ";
-        query += "text=" + connection.escape(data.text) + " ";
-        query += "WHERE id='" + data.id + "'";
-        connection.query(query, callback);
-    },
-    get: function(callback) {
-        var query = "SELECT * FROM articles ORDER BY id DESC";
-        connection.query(query, function(err, rows, fields) {
+        connection = mysql.createConnection({
+            host: mysql_host,
+            user: mysql_user,
+            password: mysql_password
+        });
+        connection.connect(function(err) {
             if(err) {
-                throw new Error("Error getting.");
+                throw new Error("Can't connect to MySQL server.");
             } else {
-                callback(rows);
+                connection.query("USE " + mysql_database, function(err, rows, fields) {
+                    if(err) {
+                        throw new Error("Missing database.");
+                    } else {
+                        console.log("Successfully selected database.");
+                    }
+                })
             }
         });
-    },
-    remove: function(id, callback) {
-        var query = "DELETE FROM articles WHERE id='" + id + "'";
-        connection.query(query, callback);
+        return {
+            add: function(data, callback) {
+                var date = new Date();
+                var query = "";
+                query += "INSERT INTO articles (title, text, date) VALUES (";
+                query += connection.escape(data.title) + ", ";
+                query += connection.escape(data.text) + ", ";
+                query += "'" + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "'";
+                query += ")";
+                connection.query(query, callback);
+            },
+            update: function(data, callback) {
+                var query = "UPDATE articles SET ";
+                query += "title=" + connection.escape(data.title) + ", ";
+                query += "text=" + connection.escape(data.text) + " ";
+                query += "WHERE id='" + data.id + "'";
+                connection.query(query, callback);
+            },
+            get: function(callback) {
+                var query = "SELECT * FROM articles ORDER BY id DESC";
+                connection.query(query, function(err, rows, fields) {
+                    if(err) {
+                        throw new Error("Error getting rows from 'articles'");
+                    } else {
+                        callback(rows);
+                    }
+                });
+            },
+            remove: function(id, callback) {
+                var query = "DELETE FROM articles WHERE id='" + id + "'";
+                connection.query(query, callback);
+            }
+        }
     }
 }
